61f28ce7bd0a640ae91b260ab476129afa09bf63
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-24 17:52:36 -0600

    Int and address

--- DataTypes.sol
+++ DataTypes.sol
@@ -11,4 +11,21 @@ contract Primitives {
     // // uint16  ranges from 0 to 2 ** 256 - 1 or 0 - 1.157920892E77
     uint public u = 1.157920892E77;
     uint256 public u256 = 1.157920892E77;
+
+        /*
+    Negative numbers are allowed for int types.
+    Like uint, different ranges are available from int8 to int256
+    
+    int256 ranges from -2 ** 255 to 2 ** 255 - 1
+    int128 ranges from -2 ** 127 to 2 ** 127 - 1
+    */
+    int8 public i8 = -1;
+    int public i256 = 456;
+    int public i = -123; // int is same as int256
+
+    // minimum and maximum of int
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+
+    address public addr = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
 }
