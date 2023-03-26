a09cc31d8c366ac257c7f25edd1c2907d74269a7
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-26 11:31:34 -0600

    Loopers

---
+++ Loopees.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Loop {
+    function loop() public view {
+        // for loop
+        for (uint i = 0; i < 10; i++) {
+            console.log(i);
+            if (i == 3) {
+                console.log("i == 3 continue");
+                // Skip to next iteration with continue
+                continue;
+            }
+            if (i == 5) {
+                console.log("i == 5, break");
+                // Exit loop with break
+                break;
+            }
+        }
+
+        // while loop
+        uint j;
+        while (j < 10) {
+            j++;
+        }
+    }
+}
