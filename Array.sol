9e58a65cf7360ce1e4913c2c54142899ccd1915c
Dmitry Sky <dmitry.sky@icloud.com>
2023-03-27 14:39:24 -0600

    Remove element from array by shifting

--- Array.sol
+++ Array.sol
@@ -36,4 +36,29 @@ contract Array {
         uint[] memory a = new uint[](5);
         return a[0];
     }
+
+    function removeByShifting(uint index) public {
+        require(index < arr.length, "index out of bound");
+
+        for (uint i = index; i < arr.length; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr.pop();
+    }
+
+    function test() external {
+        arr = [1, 2, 3, 4, 5];
+        remove(2);
+        // [1, 2, 4, 5]
+        assert(arr[0] == 1);
+        assert(arr[1] == 2);
+        assert(arr[2] == 4);
+        assert(arr[3] == 5);
+        assert(arr.length == 4);
+
+        arr = [1];
+        remove(0);
+        // []
+        assert(arr.length == 0);
+    }
 }
